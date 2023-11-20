9c198070075f0147bbb102e6218dd2e0c2069289
Félix Sánchez-Tembleque <felixstdp@gmail.com>
2023-11-20 10:48:58 +0100

    Create raptor_pdc_v2.ino

---
+++ raptor_pro/raptor_pdc_v2.ino
@@ -0,0 +1,17 @@
+#define IR1 A3
+#define IR2 A2
+#define IR3 A6
+#define IR4 A1
+#define IR5 A0
+#define IR6 A7
+#define IR7 A8
+#define IR8 A9
+#define IR 3
+
+#define STBY 
+#define AI1 7
+#define AI2 16
+#define PWMA 10
+#define BI1 14
+#define BI2 15
+#define PWMB 5
